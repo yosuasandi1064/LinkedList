43782955eb48a9fca9a343ded54be7a02295e0ae
LofannyWD1010 <38532561+LofannyWD1010@users.noreply.github.com>
2018-04-30 23:27:42 +0700

    Update SLLC.cpp

--- SLLC.cpp
+++ SLLC.cpp
@@ -25,6 +25,19 @@ void InsFront(int nilai){
         baru->next=head;
         head=baru;
     }
+    cout<<"\n\n Data "<<nilai<<" dimasukkan ke dalam list \n\n";
+}
+
+void Display(){
+    node *baru=new node;
+    baru=head;
+    if (isEmpty()==0){
+        cout<<"\n\n Data yang ada dalam list \n\n";
+    while(baru!=NULL){
+        cout<<baru->data<<" ";
+        baru=baru->next;
+        }
+    }
 }
 
 void InsBack(int nilai){
@@ -42,19 +55,7 @@ void InsBack(int nilai){
         }
         bantu->next=baru;
     }
-    cout<<"\n\n Data dimasukkan ke dalam list \n\n";
-}
-
-void Display(){
-    node *baru=new node;
-    baru=head;
-    if (isEmpty()==0){
-        cout<<"\n\n Data yang ada dalam list \n\n";
-    while(baru!=NULL){
-        cout<<baru->data<<" ";
-        baru=baru->next;
-    	}
-    }
+    cout<<"\n\n Data "<<nilai<<" dimasukkan ke dalam list \n\n";
 }
 
 void DelFront()
@@ -77,4 +78,19 @@ void DelBack(){
     tail=before;
     before->next=NULL;
     delete after;
+}
+
+void InsertPos(int letak, int nilai){
+    node *before=new node;
+    node *after=new node;
+    node *baru=new node;
+    after=head;
+    for(int i=1;i<letak;i++)
+    {
+      before=after;
+      after=after->next;
+    }
+    baru->data=nilai;
+    before->next=baru;
+    baru->next=after;
 }
