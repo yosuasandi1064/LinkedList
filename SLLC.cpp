c4a09f820d25df5f3f0b078df411b767269ee71f
Yosua Sandi Susanto <38531813+yosuasandi1064@users.noreply.github.com>
2018-04-30 21:43:54 +0700

    Add files via upload

---
+++ SLLC.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+};
+    node *head;
+
+int linkedlist() {
+	if (head == NULL)
+	return 1;
+	else return 0; }
+
+void InsFront(int nilai){
+    node *baru;
+    baru = new node;
+    baru->data=nilai;
+    baru->next=NULL;
+    if(linkedlist()==1){
+        head = baru;
+        head->next=NULL;
+    }else{
+        baru->next=head;
+        head=baru;
+    }
+
+}
+
+int main (){
+    int nilai;
+    InsFront(nilai);
+}
