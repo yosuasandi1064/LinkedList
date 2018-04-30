0616cdf3b5b6d05bda3a2a92b000af068fa1dbd6
LofannyWD1010 <38532561+LofannyWD1010@users.noreply.github.com>
2018-04-30 22:04:38 +0700

    Update SLLC.cpp

--- SLLC.cpp
+++ SLLC.cpp
@@ -7,25 +7,39 @@ struct node{
 };
     node *head;
 
-int linkedlist() {
+int isEmpty() {
 	if (head == NULL)
 	return 1;
-	else return 0; }
+	else return 0;
+}
 
 void InsFront(int nilai){
     node *baru;
     baru = new node;
     baru->data=nilai;
     baru->next=NULL;
-    if(linkedlist()==1){
+    if(isEmpty()==1){
         head = baru;
         head->next=NULL;
     }else{
         baru->next=head;
         head=baru;
     }
-
 }
+
+void Display(){
+    node *baru=new node;
+    baru=head;
+    if (isEmpty()==0){
+        cout<<"\n\n Data yang ada dalam list \n\n";
+    while(baru!=NULL){
+        cout<<baru->data<<" ";
+        baru=baru->next;
+    }
+    }else
+        cout<<"masih kosong";
+}
+
 int main (){
     int nilai;
     InsFront(nilai);
